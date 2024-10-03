5f4278dfa3597437a13650851d7082fa5dff27e3
renardelyon <renard.elyon.r@gmail.com>
2024-10-03 17:12:54 +0700

    Adjust Transformation Section based on design

--- src/components/TransformationSection.tsx
+++ src/components/TransformationSection.tsx
@@ -1,19 +1,24 @@
+import CountUp from "react-countup";
 import Section from "./Section";
+import { RiArticleFill } from "react-icons/ri";
+import { IoIosStar } from "react-icons/io";
+import { MdSwapHorizontalCircle } from "react-icons/md";
+import { BiWorld } from "react-icons/bi";
 
 const TransformationSection = () => {
   return (
     <Section
       id="transformation"
-      className="!pt-1 !pb-1 bg-gradient-to-b from-color-14 to-color-15 text-black"
+      className="!pt-1 !pb-1 bg-gradient-to-b from-color-14 from-[-30%] via-color-15 via-80% to-color-14 text-black"
     >
-      <div className="container relative">
+      <div className="container relative mt-20 xl:!max-w-[105rem]">
         <div className="py-16 px-4">
           {/* First Section: Main Heading */}
-          <div className="text-center mb-12 max-w-[62rem] mx-auto">
-            <h1 className="text-4xl md:text-5xl font-bold mb-4 font-montserrat">
+          <div className="text-center mb-20 max-w-[72rem] mx-auto">
+            <h1 className="text-4xl lg:text-5xl font-bold mb-8 font-montserrat">
               BECOME PART OF A GLOBAL TRANSFORMATION MOVEMENT
             </h1>
-            <p className="text-gray-600 max-w-2xl mx-auto font-open-sans text-lg font-semibold">
+            <p className="text-gray-600 max-w-6xl mx-auto font-open-sans text-lg md:text-2xl font-semibold">
               This extraordinary once-a-year event is your gateway to joining a
               worldwide community of visionaries, change-makers, and conscious
               leaders. By participating, you're not just transforming your own
@@ -24,53 +29,110 @@ const TransformationSection = () => {
 
           {/* Metrics Section */}
           <div
-            className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mb-12 text-black
-              text-left font-montserrat ml-20 -mr-10
+            className={`grid grid-cols-1 md:grid-cols-2 min-[1520px]:grid-cols-4 gap-8 mb-12 text-black
+              text-left font-montserrat  min-[1520px]:ml-20 min-[1520px]:-mr-10 max-w-8xl mx-12 sm-mx-0
               `}
           >
             {[
-              { title: "250k+", subtitle: "Success Stories" },
-              { title: "Rated 4.8", subtitle: "From 365 Google Reviews" },
-              { title: "4,000+", subtitle: "Transformative Sessions" },
-              { title: "30+", subtitle: "Countries United" },
-            ].map(({ title, subtitle }) => (
-              <div className="border-l-8 border-l-red-500  pl-5">
-                <p className="text-3xl font-bold">{title}</p>
-                <p className="text-gray-600">{subtitle}</p>
+              {
+                title: (
+                  <div className="flex items-center">
+                    <RiArticleFill className="mr-2" />
+                    <CountUp
+                      end={250}
+                      suffix="k+"
+                      enableScrollSpy
+                      duration={3}
+                    />
+                  </div>
+                ),
+                subtitle: "Success Stories",
+              },
+              {
+                title: (
+                  <div className="flex items-center">
+                    <IoIosStar className="mr-2" />
+                    <p>Rated 4.8</p>
+                  </div>
+                ),
+                subtitle: "From 365 Google Reviews",
+              },
+              {
+                title: (
+                  <div className="flex items-center">
+                    <MdSwapHorizontalCircle className="mr-2" />
+                    <CountUp
+                      end={4000}
+                      suffix="+"
+                      enableScrollSpy
+                      duration={3}
+                    />
+                  </div>
+                ),
+                subtitle: "Transformative Sessions",
+              },
+              {
+                title: (
+                  <>
+                    <div className="flex items-center">
+                      <BiWorld className="mr-2" />
+                      <CountUp
+                        end={30}
+                        suffix="+"
+                        enableScrollSpy
+                        duration={3}
+                      />
+                    </div>
+                  </>
+                ),
+                subtitle: "Countries United",
+              },
+            ].map(({ title, subtitle }, index) => (
+              <div
+                className={`border-l-[12px] md:ml-32 min-[1520px]:ml-0 border-l-red-500 pl-5 ${
+                  index == 1 && "min-[1520px]:-ml-14"
+                }`}
+              >
+                <p className="text-3xl min-[1520px]:text-5xl font-bold">
+                  {title}
+                </p>
+                <p className="text-lg md:text-2xl font-normal pt-2">
+                  {subtitle}
+                </p>
               </div>
             ))}
           </div>
 
           {/* Partners Section */}
-          <div className="flex items-center justify-center mb-16">
-            <div className="bg-white p-4 rounded-md inline-block">
+          <div className="flex items-center justify-center mb-16 mt-15">
+            <div className="flex flex-col space-y-10 sm:space-y-0 justify-center items-center sm:inline-block bg-white px-10 py-6 rounded-3xl ">
               <img
                 src="/Q ACADEMY - Black 1.png"
                 alt="Quantum Academy"
-                className="inline-block w-48 mr-4"
+                className="inline-block w-[160px] md:w-[200px] xl:w-[260px] md:mr-16"
               />
               <img
                 src="/OFFICIAL RESEARCH PARTNERS.png"
                 alt="Partner 1"
-                className="inline-block w-36 mr-4"
+                className="inline-block w-[140px] md:w-[180px] xl:w-[228px] md:mr-16"
               />
               <img
                 src="/James-cook-u.png"
                 alt="Partner 2"
-                className="inline-block w-36"
+                className="inline-block w-[120px] md:w-[160px] xl:w-[175px]"
               />
             </div>
           </div>
 
           {/* Second Section: Subheading and Text */}
-          <div className="text-center mb-12 font-open-sans">
-            <h2 className="text-4xl md:text-5xl font-bold mb-4 !font-montserrat">
+          <div className="text-center mb-12 font-open-sans mt-48 max-w-6xl mx-auto">
+            <h2 className="!leading-[1.15] text-4xl md:text-5xl font-bold mb-10 !font-montserrat">
               QUANTUM LEAPING WILL REWRITE YOUR REALITY AT THE SUBATOMIC LEVEL
             </h2>
-            <p className="border-l-8 pl-4 border-l-red-500 italic mb-4 text-xl font-bold !font-montserrat inline-block">
+            <p className="border-l-8 mb-10 pl-4 border-l-red-500 italic text-2xl md:text-3xl font-bold !font-montserrat inline-block">
               "We don't have to go without, when we go within"
             </p>
-            <p className="text-lg max-w-3xl mx-auto">
+            <p className="text-lg md:text-2xl mx-auto">
               Unlike traditional self-help methods, Quantum Leaping is a
               breakthrough approach that rewrites your reality at the subatomic
               level, unlocking the 99.99% of untapped potential within you. Yes
@@ -80,10 +142,10 @@ const TransformationSection = () => {
               Energy, by simply removing the boundaries and blockages from our
               body, mind and soul.
             </p>
-            <p className=" text-lg max-w-2xl mx-auto mt-6">
+            <p className=" text-lg md:text-2xl max-w-2xl mx-auto mt-6">
               Would you take the leap?
             </p>
-            <p className="text-lg max-w-3xl mx-auto mt-6">
+            <p className="text-lg md:text-2xl mx-auto mt-6">
               What if ancient and timeless techniques such as meditation,
               breathwork, advanced self-development and energy mastery could
               help you harness the power of the Quantum field to propel your
